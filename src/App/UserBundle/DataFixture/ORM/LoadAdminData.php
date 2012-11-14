22a350784bb98be4928547eb9b0b78fc85e305da
Sergey Lunev <sergey@lunyov.ru>
2012-11-14 15:31:58 +0400

    Fix #4: Demo admin

---
+++ src/App/UserBundle/DataFixture/ORM/LoadAdminData.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadAdminData extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+    private $container;
+
+    public function load(ObjectManager $manager)
+    {
+        $manipulator = $this->container->get('fos_user.user_manager');
+
+        /** @var $admin example superadmin */
+        $admin = $manipulator->createUser();
+        $admin->setUsername('admin');
+        $admin->setEmail('exadmin@email.com');
+        $admin->setPlainPassword('password');
+        $admin->setEnabled(true);
+        $admin->setSuperAdmin(true);
+        $manipulator->updateUser($admin);
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
